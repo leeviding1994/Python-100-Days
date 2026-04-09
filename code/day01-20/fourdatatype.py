5c3209f3dd8355544416ff634b0bda8fecf0d6a6
LeeviDing <noah.ding@sunpuretech.com>
2026-04-09 09:47:50 +0800

    mycode提交

---
+++ code/day01-20/fourdatatype.py
@@ -0,0 +1,8 @@
+a = 100
+b = 123.45
+c = 'hello, world'
+d = True
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'str'>
+print(type(d))  # <class 'bool'>
